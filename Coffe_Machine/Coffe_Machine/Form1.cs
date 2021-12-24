1725baae7276516bbe5aff96c5ae6217a75e2b42
Thanathip.T <96216468+JJWER@users.noreply.github.com>
2021-12-24 16:41:29 +0700

    Coffee_Machine

---
+++ Coffe_Machine/Coffe_Machine/Form1.cs
@@ -0,0 +1,25 @@
+class Coffee_Machine{
+    int Espresso;
+    int CafeAmericano;
+    int Latte;
+    int Cappuccino;
+    int CafeMacha;
+    int Macchiato;
+    int FlatWhite;
+    int Affogato;
+    void Coffe_Machine(int Espresso, int CafeAmericano, int Latte, int Cappuccino,
+    int CafeMacha, int Macchiato, int FaltWhite, int Affogato)
+    { }
+    void buyEspresso(int amount) { }
+    void buyCafeAmericano(int amount) { }
+    void buyLatte(int amount) { }
+    void buyCappuccino(int amount) { }
+    void buyCafeMacha(int amount) { }
+    void buyMacchiato(int amount) { }
+    void buyFlatWhite(int amount) { }
+    void buyAffogato(int amount) { }
+    void showStock() { }
+    void increaseStock(int Espresso, int CafeAmericano, int Latte, int Cappuccino,
+    int CafeMacha, int Macchiato, int FaltWhite, int Affogato)
+    { }
+}
